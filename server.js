67b690d75082e4160bfd5e8212f43aca49930f81
crunchi <33149003+crunchi@users.noreply.github.com>
2021-12-03 04:24:53 +0100

    first routes tewts

--- server.js
+++ server.js
@@ -2,18 +2,23 @@ const express = require('express')
 const app = express()
 const port = 3001
 
-const {Sauveteur, Sauve, Evenement} = require('./bddConnection')
+const {Sauveteur, Sauve, Evenement,Utilisateur} = require('./bddConnection')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/sauveteurs',async (req,res)=>{
+    s = new Sauveteur
+    var [ret, meta] = await s.findAll()
+    res.send(ret)
+})
+app.get('/sauves',async (req,res)=>{
+    s = new Sauve
+    var [ret, meta] = await s.findAll()
+    res.send(ret)
+})
 
-app.listen(port, async () => {
-    var s = new Sauveteur
-    var [res,meta] = await s.findByName("Romain mas")
-    console.log(res)
-    
-      
+app.listen(port, async () => {    
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
